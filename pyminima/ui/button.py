e13b0094a943223d34321ea4614c8982839cb9a1
Gabriel <bielrammos@gmail.com>
2026-04-21 22:48:19 -0300

    rename to pypi

---
+++ pyminima/ui/button.py
@@ -0,0 +1,10 @@
+from pyminima.ui.ui_element import UIElement
+
+
+class Button(UIElement):
+    """
+    Semantic class representing interactive button elements (e.g., <button>, <input type="submit">).
+    Inherits all core actions from Widget.
+    """
+
+    pass
---
+++ pyminima/ui/image.py
@@ -0,0 +1,10 @@
+from pyminima.ui.ui_element import UIElement
+
+
+class Image(UIElement):
+    """
+    Semantic class representing image elements (e.g., <img> tags).
+    Inherits all core actions from Widget.
+    """
+
+    pass
